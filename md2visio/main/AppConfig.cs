3fca57068661d0ba4159160dcc8a7829d8a75e1b
Megre <renhao.x@seu.edu.cn>
2025-03-14 13:02:52 +0800

    md2visio params
    md2visio /I MD_FILE /O OUTPUT_PATH [/V] [/Y]
        /I    Specify the path of the input file (.md)
        /O  Specify the path/folder of the output file (.vsdx)
        /V  Show the Visio application while drawing (optional, default is invisible)
        /Y  Quietly overwrite the existing output file (optional, by default requires user confirmation

---
+++ md2visio/main/AppConfig.cs
@@ -0,0 +1,48 @@
+﻿namespace md2visio.main
+{
+    internal class AppConfig
+    {
+        public static readonly AppConfig Instance = new AppConfig();
+
+        public string InputFile { get; set; } = string.Empty;
+        public string OutputPath { get; set; } = string.Empty;
+        public bool Visible { get; set; } = false; 
+        public bool Quiet { get; set; } = false;
+        public bool Debug { get; set; } = false;
+
+        AppConfig() { }
+
+        public bool ParseArgs(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+                if(arg=="/I" && i + 1 < args.Length)
+                    InputFile = args[++i];
+                else if(arg=="/O" && i + 1 < args.Length)
+                    OutputPath = args[++i];
+                else if(arg=="/Y") 
+                    Quiet = true;
+                else if(arg=="/V")
+                    Visible = true;
+                else if(arg=="/?")
+                    ShowHelp();
+                else if (arg == "/D")
+                    Debug = true;
+            }
+
+            return InputFile != string.Empty && 
+                OutputPath != string.Empty;
+        }
+
+        void ShowHelp()
+        {
+            Console.WriteLine(
+                "md2visio /I MD_FILE /O OUTPUT_PATH [/V] [/Y]\n\n" +
+                "/I\tSpecify the path of the input file (.md)\n" +
+                "/O\tSpecify the path/folder of the output file (.vsdx)\n" +
+                "/V\tShow the Visio application while drawing (optional, default is invisible)\n" +
+                "/Y\tQuietly overwrite the existing output file (optional, by default requires user confirmation");
+        }
+    }
+}
---
+++ md2visio/main/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using md2visio.struc.figure;
+using md2visio.mermaid._cmn;
+using md2visio.main;
+
+AppConfig config = AppConfig.Instance;
+if (!config.ParseArgs(args)) return;
+
+try
+{
+    //string inputFile = @"C:\Users\Megre\Documents\GitHub\md2visio\md2visio\test\对应位置的载荷.md";
+    //string inputFile = @"C:\Users\Megre\Documents\GitHub\md2visio\md2visio\test\journey.md";
+    //string outputPath = @"C:\Users\Megre\Desktop\";
+
+    SynContext synContext = new SynContext(config.InputFile);
+    //new SynStateMachine(synContext).Start();
+    SttMermaidStart.Run(synContext);
+    if(config.Debug) 
+        Console.Write(synContext.ToString());
+
+    new FigureBuilderFactory(synContext.NewSttIterator()).Build(config.OutputPath);
+}
+catch(Exception ex)
+{
+    if (config.Debug) throw;
+    else Console.Error.WriteLine(ex.Message);
+}

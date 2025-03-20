9d070b90d14114069d0c4c12822e24bedf8a92e4
Megre <renhao.x@seu.edu.cn>
2025-03-20 23:56:48 +0800

    regression testing

---
+++ md2visio/main/AppTest.cs
@@ -0,0 +1,27 @@
+﻿using md2visio.mermaid.cmn;
+using md2visio.struc.figure;
+using md2visio.vsdx.@base;
+
+namespace md2visio.main
+{
+    internal class AppTest
+    {
+        string testDir = @"C:\Users\Megre\Documents\GitHub\md2visio\md2visio\test";
+        string[] files = ["graph", "journey", "packet", "pie", "xy"];
+        string outputPath = @"C:\Users\Megre\Desktop";
+
+        public void Test()
+        {
+            AppConfig config = AppConfig.Instance;
+            foreach (string file in files)
+            {
+                SynContext synContext = new(@$"{testDir}\{file}.md");
+                SttMermaidStart.Run(synContext);
+                Console.Write(synContext.ToString());
+
+                new FigureBuilderFactory(synContext.NewSttIterator()).Build(outputPath);
+            }
+            VBuilder.VisioApp.Quit();
+        }
+    }
+}
